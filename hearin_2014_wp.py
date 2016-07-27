823b1f501a5a96c661c01dcf65edec79d6b8d84f
Duncan Campbell <duncan.alan.campbell@gmail.com>
2016-07-27 13:00:55 -0400

    converted mstar thresholds into h=1

--- hearin_2014_wp.py
+++ hearin_2014_wp.py
@@ -15,7 +15,7 @@ import numpy as np
 __all__ = ['hearin_2014_wp']
 __author__=['Duncan Campbell']
 
-def hearin_2014_wp(mstar_thresh=10**9.8, sample='all'):
+def hearin_2014_wp(mstar_thresh=10**9.49, sample='all'):
     """
     projected two point correlation function measurements from Hearin et al. 2014
     
@@ -25,6 +25,7 @@ def hearin_2014_wp(mstar_thresh=10**9.8, sample='all'):
     ----------
     mstar_thresh : float
          minimum stellar mass of the threshold sample in :math:`h^{-2}M_{\odot}`
+         e.g. 9.49, 9.89, 10.29 (converted to h=1).
     
     sample : string
         string indicating sample used in the wp calculation:
@@ -33,12 +34,14 @@ def hearin_2014_wp(mstar_thresh=10**9.8, sample='all'):
     Returns
     -------
     measurement : numpy.ndarray
-        array of shape (2,14), where the first row is rp in :math:`h^-1` Mpc, and 
+        array of shape (2,15), where the first row is rp in :math:`h^-1` Mpc, and 
         the second row is wp in :math:`h^-1` Mpc.
     
-    err : numpy.matrix
-        err on measurement
+    err : numpy.array
+        error on the wp measurement
     """
+    
+    littleh=0.7
     
     #get files for specified sample
     if sample == 'all':
@@ -51,14 +54,26 @@ def hearin_2014_wp(mstar_thresh=10**9.8, sample='all'):
         msg = ("sample not recognized.")
         raise ValueError(msg)
     
+    #what are the mass thresholds in h=1?
+    mstar_thresholds = np.array([10.0**9.8, 10.0**10.2, 10.0**10.6]) * littleh**2.0
+    mstar_thresholds = np.log10(mstar_thresholds)
+    
+    #find nearest to the input value
     mstar_thresh = np.log10(mstar_thresh)
+    mask = np.isclose(mstar_thresh, mstar_thresholds, atol=0.01)
+    
+    if np.any(mask):
+        mstar_thresh = mstar_thresholds[mask]
+    else:
+        msg = ("mass threshold not with 0.01 dex of an available threshold.")
+        raise ValueError(msg)
     
     #get files for specified stellar mass bin
-    if mstar_thresh==9.8:
+    if mask[0]:
         column = 1
-    elif mstar_thresh==10.2:
+    elif mask[1]:
         column = 3
-    elif mstar_thresh==10.6:
+    elif mask[2]:
         column = 5
     else:
         msg = ("requested mass threshold not available.")
@@ -74,6 +89,11 @@ def hearin_2014_wp(mstar_thresh=10**9.8, sample='all'):
     sigma = np.array(data.columns[column+1])
     
     measurement = np.vstack((rp,wp))
+    
+    #convert to h=1
+    measurement[0] = measurement[0,:]*littleh
+    measurement[0] = measurement[1,:]*littleh
+    sigma = sigma*littleh
     
     return measurement, sigma
     
